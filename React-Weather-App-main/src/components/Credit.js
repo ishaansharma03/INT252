6646470455560f295f32fbf33530d92fd8f5f43e
Ishaan Sharma <ishaantaker117@gmail.com>
2024-11-13 22:26:01 +0530

    first upload

---
+++ React-Weather-App-main/src/components/Credit.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+     <a href="https://github.com/ishaansharma03/INT252">Project link to Github.</a>
+    </p>
+  </div>
+  );
+}
